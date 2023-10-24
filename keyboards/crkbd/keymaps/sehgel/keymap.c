da87dba4f0022678d4d466f862d48c034a4de1a6
Sehgel <sehgel.dev@gmail.com>
2023-10-23 22:50:19 -0300

    Fixed gameplay numpad

--- keyboards/crkbd/keymaps/sehgel/keymap.c
+++ keyboards/crkbd/keymaps/sehgel/keymap.c
@@ -169,11 +169,11 @@ KC_LSFT,XXXXXXX,UNICODE_MODE_WINC,UNICODE_MODE_LNX,UNICODE_MODE_MAC, XXXXXXX,   
                     //NUMPAD2 LAYER
                     [GAMEPLAY_NUMPAD] = LAYOUT_split_3x6_3(
                     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-                        XXXXXXX, XXXXXXX,    KC_1,    KC_2,    KC_3,   KC_F1,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                         KC_ESC,    KC_1,    KC_2,    KC_3,   KC_F1, KC_BSPC,                        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
                     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-                        XXXXXXX, XXXXXXX,    KC_4,    KC_5,    KC_6,   KC_F2,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                        XXXXXXX,    KC_4,    KC_5,    KC_6,   KC_F2, XXXXXXX,                        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
                     //|--------+--------+--------+--------+--------+--------|       NUMPAD2      |--------+--------+--------+--------+--------+--------|
-                        KC_LSFT, XXXXXXX,    KC_7,    KC_8,    KC_9,    KC_0,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
+                        KC_LSFT,    KC_7,    KC_8,    KC_9,    KC_0,  KC_ENT,                    TO_QWERTY, TO_DVORAK, TO_WORKMAN, TO_COLEMAK_DH, TO_BEAKL27, KC_RSFT,
                     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
                                                             KC_LALT, KC_RALT,  KC_SPC,   KC_ENT,   KC_LALT, KC_RALT
                                                         //`--------------------------'  `--------------------------'
