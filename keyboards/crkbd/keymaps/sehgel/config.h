3640388879ead1a1b16420e8a991ce2429adb1e4
Sehgel <sehgel.dev@gmail.com>
2024-03-04 11:52:35 -0300

    Add permisive hold

--- keyboards/crkbd/keymaps/sehgel/config.h
+++ keyboards/crkbd/keymaps/sehgel/config.h
@@ -33,6 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define MASTER_LEFT
 #define MASTER_RIGHT
+#define PERMISSIVE_HOLD
 #define TAPPING_TERM 150
 /*#define SPLIT_WPM_ENABLE
 #define SPLIT_LED_STATE_ENABLE
