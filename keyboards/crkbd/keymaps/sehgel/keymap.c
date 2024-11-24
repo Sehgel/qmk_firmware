8b04c97870e84a5073d158f4917bcf92f1c87e1f
Sehgel <sehgel.dev@gmail.com>
2024-11-24 18:27:53 -0300

    Swapped some symbols

--- keyboards/crkbd/keymaps/sehgel/keymap.c
+++ keyboards/crkbd/keymaps/sehgel/keymap.c
@@ -158,9 +158,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
                          KC_ESC,  KC_DEL,   KC_P7,   KC_P8,   KC_P9, KC_BSPC,                      KC_PSCR, RGB_HUI, RGB_SAI, RGB_VAI, KC_VOLU, UNICODE_MODE_WINC,
                     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-                          KC_P0, KC_PSLS,   KC_P4,   KC_P5,   KC_P6, KC_PAST,                      KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, UNICODE_MODE_LNX,
+                          KC_P0, KC_PAST,   KC_P4,   KC_P5,   KC_P6, KC_PSLS,                      KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, UNICODE_MODE_LNX,
                     //|--------+--------+--------+--------+--------+--------|       NUMPAD       |--------+--------+--------+--------+--------+--------|
-                         KC_DOT, KC_PMNS,   KC_P1,   KC_P2,   KC_P3, KC_PPLS,         TO_QWERTY, TO_DVORAK,TO_WORKMAN,TO_COLEMAK_DH,TO_BEAKL43, UNICODE_MODE_MAC,
+                         KC_DOT, KC_PPLS,   KC_P1,   KC_P2,   KC_P3, KC_PMNS,         TO_QWERTY, TO_DVORAK,TO_WORKMAN,TO_COLEMAK_DH,TO_BEAKL43, UNICODE_MODE_MAC,
                     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
                                                              KC_ENT, KC_LALT, KC_SPC,      KC_ENT, KC_LSFT, MO(NUMPAD2)
                                                         //`--------------------------'  `--------------------------'
@@ -179,30 +179,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                         //`--------------------------'  `--------------------------'
                     ),
 
-                    //GAMEPLAY LAYER TODO: Change the Fs from the left side.
+                    //GAMEPLAY LAYER
                     [GAMEPLAY_NUMPAD] = LAYOUT_split_3x6_3(
                     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
                          KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
                     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
                         KC_PMNS,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
                     //|--------+--------+--------+--------+--------+--------|  GAMEPLAY NUMPAD   |--------+--------+--------+--------+--------+--------|
-                        KC_LSFT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_ENT,           TO_QWERTY, TO_DVORAK,TO_WORKMAN,TO_COLEMAK_DH,TO_BEAKL43, XXXXXXX,
+                        KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           TO_QWERTY, TO_DVORAK,TO_WORKMAN,TO_COLEMAK_DH,TO_BEAKL43,XXXXXXX,
                     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
                                                             KC_LALT, KC_RALT,  KC_SPC,   KC_ENT,   KC_LALT, KC_RALT
                                                         //`--------------------------'  `--------------------------'
                     ),
 
-                    //SYMBOLS LAYER TODO: Minus in right side is duplicated, find a replacement
+                    //SYMBOLS
                     [SYMBOLS] = LAYOUT_split_3x6_3(
                     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-               X(L_EXCLAMATION), KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_MINS, KC_AMPR, KC_LPRN, KC_RPRN, KC_ASTR, KC_BSPC,
+               X(L_EXCLAMATION), KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      XXXXXXX, KC_AMPR, KC_LPRN, KC_RPRN, KC_ASTR, KC_BSPC,
                     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-                        KC_TILD, KC_BSLS, KC_LABK, KC_RABK, KC_PSLS, KC_UNDS,                     KC_MINUS,  KC_EQL, KC_LBRC, KC_RBRC, KC_PPLS, KC_CIRC,
+                        KC_UNDS, KC_BSLS, KC_LABK, KC_RABK, KC_PSLS,KC_MINUS,                      KC_CIRC,  KC_EQL, KC_LBRC, KC_RBRC, KC_PPLS, KC_TILD,
                     //|--------+--------+--------+--------+--------+--------|       SYMBOLS      |--------+--------+--------+--------+--------+--------|
-               KC_LSFT, X(L_EXCLAMATION),X(L_QUESTION),ENIE,KC_QUES, KC_EXLM,                       KC_GRV, KC_PIPE, KC_LCBR, KC_RCBR,EMOJI_KEY, KC_RSFT,
+               KC_LSFT, X(L_EXCLAMATION),X(L_QUESTION),KC_QUES, KC_EXLM, XXXXXXX,                   KC_GRV, KC_PIPE, KC_LCBR, KC_RCBR,EMOJI_KEY, KC_RSFT,
                     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-                                                        XXXXXXX ,   XXXXXXX,   KC_SPC,  KC_ENT,   XXXXXXX,   KC_RALT
-                                                        //`--------------------------'  `--------------------------'
+                                                        ENIE ,   XXXXXXX,   KC_SPC,  KC_ENT,   XXXXXXX,   KC_RALT
+                                                        //`--------------------------'  `--------------------------¿
                     ),
 
                     //SHORTCUTS LAYER
