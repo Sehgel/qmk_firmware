9a0d256d4e333543094b399d7fb790cced13110c
Sehgel <sehgel.dev@gmail.com>
2025-09-27 21:15:04 -0300

    Add print screen to Functions layer

--- keyboards/planck/keymaps/sehgel/keymap.c
+++ keyboards/planck/keymaps/sehgel/keymap.c
@@ -289,7 +289,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//--------------------------------------------------------------------------------------------------------------------
 	KC_MUTE,KC_VOLD,KC_MPRV,KC_MPLY,KC_MNXT,KC_VOLU,/*------------------*/XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_APP ,
 	//--------------------------------------------------------------------------------------------------------------------
-	XXXXXXX,XXXXXXX,KC_LALT,XXXXXXX,XXXXXXX,KC_SPC ,/*------------------*/KC_ENT ,KC_LALT,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX
+	XXXXXXX,XXXXXXX,KC_LALT,XXXXXXX,XXXXXXX,KC_SPC ,/*------------------*/KC_ENT ,KC_LALT,XXXXXXX,KC_PSCR,XXXXXXX,XXXXXXX
 	//--------------------------------------------------------------------------------------------------------------------
 ),
 
