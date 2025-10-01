69aecfba8743aef6d2162f0929e0b4ee9ff3c603
Sehgel <sehgel.dev@gmail.com>
2025-10-01 16:37:42 -0300

    change numpad for gaming layer

--- keyboards/planck/keymaps/sehgel/keymap.c
+++ keyboards/planck/keymaps/sehgel/keymap.c
@@ -84,13 +84,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [QWERTY] = LAYOUT_planck_grid(
 	//---------------------------------------------------------------------------------------------------------------------------------------
-	KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R      ,KC_T  ,/*------------------*/KC_Y  ,KC_U          ,KC_I         ,KC_O       ,KC_P    ,KC_BSPC,
+	KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R               ,KC_T  ,/*------------------*/KC_Y  ,KC_U          ,KC_I         ,KC_O       ,KC_P    ,KC_BSPC,
 	//---------------------------------------------------------------------------------------------------------------------------------------
-	KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F      ,KC_G  ,/*------------------*/KC_H  ,KC_J          ,KC_K         ,KC_L       ,KC_SCLN ,KC_RCTL,
+	KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F               ,KC_G  ,/*------------------*/KC_H  ,KC_J          ,KC_K         ,KC_L       ,KC_SCLN ,KC_RCTL,
 	//---------------------------------------------------------------------------------------------------------------------------------------
-	KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V      ,KC_B  ,/*------------------*/KC_N  ,KC_M          ,KC_COMM      ,KC_DOT     ,KC_QUOTE,KC_RSFT,
+	KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V               ,KC_B  ,/*------------------*/KC_N  ,KC_M          ,KC_COMM      ,KC_DOT     ,KC_QUOTE,KC_RSFT,
 	//---------------------------------------------------------------------------------------------------------------------------------------
-	XXXXXXX,XXXXXXX,KC_LALT,KC_LWIN,MO(NUMPAD),KC_SPC,/*------------------*/KC_ENT,MO(NAVIGATION),MO(FUNCTIONS),MO(SYMBOLS),XXXXXXX ,XXXXXXX
+	XXXXXXX,XXXXXXX,KC_LALT,KC_LWIN,MO(GAMEPLAY_NUMPAD),KC_SPC,/*------------------*/KC_ENT,MO(NAVIGATION),MO(FUNCTIONS),MO(SYMBOLS),XXXXXXX ,XXXXXXX
 	//---------------------------------------------------------------------------------------------------------------------------------------
 ),
 
