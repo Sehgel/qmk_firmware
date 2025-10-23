ba17bf0f993c13f07cfd7f671bd968d802589487
Sehgel <sehgel.dev@gmail.com>
2025-10-23 20:33:29 -0300

    Switch emoji key

--- keyboards/crkbd/keymaps/sehgel/keymap.c
+++ keyboards/crkbd/keymaps/sehgel/keymap.c
@@ -164,7 +164,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     //|--------+--------+--------+--------+--------+--------|       SYMBOLS      |--------+--------+--------+--------+--------+--------|
                KC_LSFT, X(L_EXCLAMATION),X(L_QUESTION),KC_QUES,KC_EXLM, ENIE,                       KC_GRV,KC_COLON, KC_LCBR, KC_RCBR,KC_MINUS, KC_RSFT,
                     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-                                                   XXXXXXX,KC_LALT, XXXXXXX,   KC_SPC,  KC_ENT,   XXXXXXX,EMOJI_KEY, XXXXXXX
+                                                   XXXXXXX,KC_LALT, XXXXXXX,   KC_SPC,  KC_ENT,   EMOJI_KEY,XXXXXXX, XXXXXXX
                                                         //`--------------------------'  `--------------------------
                     ),
 
