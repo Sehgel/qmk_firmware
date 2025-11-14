f0770ce54b065188765edb3f2de6f5668964fb6d
Sehgel <sehgel.dev@gmail.com>
2025-11-14 19:47:34 -0300

    Match function keys layer to numpad

--- keyboards/planck/keymaps/sehgel/keymap.c
+++ keyboards/planck/keymaps/sehgel/keymap.c
@@ -169,11 +169,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [FUNCTIONS]=LAYOUT_planck_grid(
 	//--------------------------------------------------------------------------------------------------------------------
-	KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,/*------------------*/KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,
+	KC_F15 ,KC_F12 ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F18 ,/*------------------*/KC_F19 ,KC_F20 ,KC_F21 ,KC_F22 ,KC_F23 ,KC_F24 ,
 	//--------------------------------------------------------------------------------------------------------------------
-	KC_F13 ,KC_F14 ,KC_F15 ,KC_F16 ,KC_F17 ,KC_F18 ,/*------------------*/KC_F19 ,KC_F20 ,KC_F21 ,KC_F22 ,KC_F23 ,KC_F24 ,
+	KC_F14 ,KC_F11 ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F17 ,/*------------------*/XXXXXXX,KC_VOLD,KC_MPLY,KC_VOLU,XXXXXXX,XXXXXXX,
 	//--------------------------------------------------------------------------------------------------------------------
-	KC_MUTE,KC_VOLD,KC_MPRV,KC_MPLY,KC_MNXT,KC_VOLU,/*------------------*/XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_APP ,
+	KC_F13 ,KC_F10 ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F16 ,/*------------------*/XXXXXXX,KC_MPRV,KC_MUTE,KC_MNXT,XXXXXXX,KC_APP ,
 	//--------------------------------------------------------------------------------------------------------------------
 	XXXXXXX,XXXXXXX,KC_LWIN,KC_LALT,XXXXXXX,KC_SPC ,/*------------------*/KC_ENT ,KC_LALT,KC_PSCR,XXXXXXX,XXXXXXX,XXXXXXX
 	//--------------------------------------------------------------------------------------------------------------------
