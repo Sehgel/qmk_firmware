eff5024793c3c706d72e88cf5537ef45d22c2822
Sehgel <sehgel.dev@gmail.com>
2026-05-18 16:50:57 -0300

    Fix QWERTY layer and revert some things

--- keyboards/crkbd/keymaps/sehgel/keymap.c
+++ keyboards/crkbd/keymaps/sehgel/keymap.c
@@ -78,9 +78,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
                         KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_RCTL,
                     //|--------+--------+--------+--------+--------+--------|       QWERTY       |--------+--------+--------+--------+--------+--------|
-                        KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_M,    KC_N, KC_COMM,  KC_DOT,KC_QUOTE, KC_RSFT,
+			KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT,KC_QUOTE, KC_RSFT,
                     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-                                                 XXXXXXX,KC_LALT, MO(GAMEPLAY_NUMPAD), KC_SPC,    KC_ENT, MO(NAVIGATION), MO(SYMBOLS),MO(FUNCTIONS)
+                                                 KC_LWIN,KC_LALT, MO(GAMEPLAY_NUMPAD), KC_SPC,    KC_ENT, MO(NAVIGATION), MO(SYMBOLS),MO(FUNCTIONS)
                                                         //`--------------------------'  `--------------------------'
                     ),
 
@@ -134,16 +134,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     //PMNS = -
                     //PAST = *
                     //PSLS = /
-                    //
                     [NUMPAD] = LAYOUT_split_3x6_4(
                     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
                          KC_ESC,  KC_DEL,   KC_P7,   KC_P8,   KC_P9, KC_BSPC,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, UNICODE_MODE_WINC,
                     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-                          KC_P0, KC_PSLS,   KC_P4,   KC_P5,   KC_P6, KC_PAST,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, UNICODE_MODE_LNX,
+                        KC_LCTL, KC_PSLS,   KC_P4,   KC_P5,   KC_P6, KC_PAST,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, UNICODE_MODE_LNX,
                     //|--------+--------+--------+--------+--------+--------|       NUMPAD       |--------+--------+--------+--------+--------+--------|
                          KC_DOT, KC_PMNS,   KC_P1,   KC_P2,   KC_P3, KC_PPLS,         TO_QWERTY, TO_DVORAK,TO_WORKMAN,TO_COLEMAK_DH,TO_BEAKL43, UNICODE_MODE_MAC,
                     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-                                                   KC_LWIN, KC_PENT, XXXXXXX,  KC_SPC,      KC_ENT, KC_LSFT, MO(NUMPAD2),XXXXXXX
+                                                     KC_P0, KC_PENT, XXXXXXX,  KC_SPC,     KC_ENT, KC_LSFT, MO(NUMPAD2),XXXXXXX
                                                         //`--------------------------'  `--------------------------'
                     ),
 
@@ -188,11 +187,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     
                     [FUNCTIONS] = LAYOUT_split_3x6_4(
                     //--------------------------------------------------------------------------------------------------------------------
-                    KC_F15 ,KC_F12 ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F18 ,/*------------------*/KC_F19 ,KC_F20 ,KC_F21 ,KC_F22 ,KC_F23 ,KC_F24 ,
+                    KC_F18 ,KC_F15 ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F12 ,/*------------------*/KC_F19 ,KC_F20 ,KC_F21 ,KC_F22 ,KC_F23 ,KC_F24 ,
                     //--------------------------------------------------------------------------------------------------------------------
-                    KC_F14 ,KC_F11 ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F17 ,/*------------------*/XXXXXXX,KC_VOLD,KC_MPLY,KC_VOLU,XXXXXXX,XXXXXXX,
+                    KC_F17 ,KC_F14 ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F11 ,/*------------------*/XXXXXXX,KC_VOLD,KC_MPLY,KC_VOLU,XXXXXXX,XXXXXXX,
                     //--------------------------------------------------------------------------------------------------------------------
-                    KC_F13 ,KC_F10 ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F16 ,/*------------------*/XXXXXXX,KC_MPRV,KC_MUTE,KC_MNXT,XXXXXXX,KC_APP ,
+                    KC_F16 ,KC_F13 ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F10 ,/*------------------*/XXXXXXX,KC_MPRV,KC_MUTE,KC_MNXT,XXXXXXX,KC_APP ,
                     //--------------------------------------------------------------------------------------------------------------------
                                     KC_LWIN,KC_LALT,XXXXXXX,KC_SPC ,/*------------------*/KC_ENT ,KC_LALT,KC_PSCR,XXXXXXX
                     //--------------------------------------------------------------------------------------------------------------------
